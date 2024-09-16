cae1da3ce85da62d4b7e5e9713c5c611f6e93fbe
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-09-17 00:18:11 +0400

    Handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 use std::time::Duration;
 use crate::protocol::DataType;
+use std::thread;
 
 use crate::error::RedisError;
 
@@ -46,6 +47,31 @@ pub(crate) fn read_message(stream: &mut TcpStream) -> Result<Option<protocol::Da
     }
 }
 
+fn server_worker(stream: &mut TcpStream) -> Result<(), anyhow::Error> {
+    stream.set_read_timeout(Some(Duration::new(1, 0)))?;
+    println!("accepted new connection");
+
+    let expected_ping_message = protocol::DataType::Array {
+        elements: vec![
+            protocol::DataType::BulkString { value: "PING".as_bytes().to_vec() }
+        ]
+    };
+
+    loop {
+        if let Some(received_message) = read_message(stream)? {
+            println!("Received: {}", String::from_utf8_lossy(&received_message.serialize()));
+            if received_message == expected_ping_message {
+                let reply = protocol::DataType::SimpleString {
+                    value: "PONG".as_bytes().to_vec()
+                }.serialize();
+                stream.write_all(&reply)?;
+                println!("Replied with pong")
+            }
+        }
+        //TODO: Terminate the connection when requested by the client
+    }
+}
+
 fn main() -> Result<(), anyhow::Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -53,27 +79,9 @@ fn main() -> Result<(), anyhow::Error> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for incoming_connection in listener.incoming() {
         let mut stream = incoming_connection?;
-        stream.set_read_timeout(Some(Duration::new(1, 0)))?;
-        println!("accepted new connection");
-
-        let expected_ping_message = protocol::DataType::Array {
-            elements: vec![
-                protocol::DataType::BulkString { value: "PING".as_bytes().to_vec() }
-            ]
-        };
-
-        loop {
-            if let Some(received_message) = read_message(&mut stream)? {
-                println!("Received: {}", String::from_utf8_lossy(&received_message.serialize()));
-                if received_message == expected_ping_message {
-                    let reply = protocol::DataType::SimpleString {
-                        value: "PONG".as_bytes().to_vec()
-                    }.serialize();
-                    stream.write_all(&reply)?;
-                    println!("Replied with pong")
-                }
-            }
-        }
+        thread::spawn(move || {
+            server_worker(&mut stream)
+        });
     }
     Ok(())
 }
