3bff378f318351a4d218d28419cefa05d9860bf3
Anton Moilanen <anton.a.moilanen@gmail.com>
2026-06-01 01:57:32 +0400

    Re-factor handle_connection in connection.rs

--- src/connection.rs
+++ src/connection.rs
@@ -48,142 +48,156 @@ pub fn handle_connection(
             );
             match &received_message {
                 DataType::Array { elements } => {
-                    let command_name: String = commands::parse_command_name(&received_message)?;
-                    let mut command: Option<Box<dyn RedisCommand>> = None;
-                    let command_name = command_name.as_str();
-
-                    // Dispatch to appropriate command handler
-                    if command_name == "ECHO" {
-                        command = Some(Box::new(Echo {
-                            message: received_message.clone(),
-                            argument: elements.get(1).cloned(),
-                        }));
-                    } else if command_name == "PING" {
-                        command = Some(Box::new(Ping {
-                            message: received_message.clone(),
-                        }));
-                    } else if command_name == "SET" {
-                        command = Some(Box::new(Set {
-                            message: received_message.clone(),
-                        }));
-                    } else if command_name == "GET" {
-                        command = Some(Box::new(Get {
-                            message: received_message.clone(),
-                        }));
-                    } else if command_name == "COMMAND" {
-                        command = Some(Box::new(Command {
-                            message: received_message.clone(),
-                        }));
-                    } else if command_name == "INFO" {
-                        command = Some(Box::new(Info {
-                            message: received_message.clone(),
-                            server_state: Arc::clone(server_state),
-                        }));
-                    } else if command_name == "REPLCONF" {
-                        command = Some(Box::new(ReplConf {
-                            message: received_message.clone(),
-                            server_state: Arc::clone(server_state),
-                        }));
-                    } else if command_name == "RPUSH" {
-                        command = Some(Box::new(RPush {
-                            message: received_message.clone(),
-                            notifier: Arc::clone(&server_state.blocking_notifier),
-                        }));
-                    } else if command_name == "LPUSH" {
-                        command = Some(Box::new(LPush {
-                            message: received_message.clone(),
-                            notifier: Arc::clone(&server_state.blocking_notifier),
-                        }));
-                    } else if command_name == "LRANGE" {
-                        command = Some(Box::new(LRange {
-                            message: received_message.clone()
-                        }));
-                    } else if command_name == "LLEN" {
-                        command = Some(Box::new(LLen {
-                            message: received_message.clone()
-                        }));
-                    } else if command_name == "LPOP" {
-                        command = Some(Box::new(LPop {
-                            message: received_message.clone()
-                        }));
-                    } else if command_name == "BLPOP" {
-                        command = Some(Box::new(BLPop {
-                            message: received_message.clone(),
-                            notifier: Arc::clone(&server_state.blocking_notifier),
-                        }));
-                    } else if command_name == "TYPE" {
-                        command = Some(Box::new(Type {
-                            message: received_message.clone()
-                        }));
-                    } else if command_name == "PSYNC" {
-                        command = Some(Box::new(PSync {
-                            message: received_message.clone(),
-                            server_state: Arc::clone(server_state),
-                        }));
-                        server_state
-                            .replica_connections
-                            .lock()
-                            .map_err(|e| anyhow!("Failed to lock replica connections: {}", e))?
-                            .push(stream.try_clone()?);
-                    }
-
-                    if let Some(command) = command {
-                        let reply = command.execute(storage)?;
-                        if should_reply || command.should_always_reply() {
-                            for message in reply.into_iter() {
-                                trace!("Sending: {:?}", message);
-                                let message_bytes = &message.serialize();
-                                trace!("which serializes to {:?}", message_bytes);
-                                stream.write_all(message_bytes)?;
-                            }
-                        }
-
-                        // Propagate write commands to replicas if this is a master
-                        let should_propagate_to_replicas =
-                            server_state.is_master() && command.is_propagated_to_replicas();
-                        if should_propagate_to_replicas {
-                            let command_bytes = command.serialize();
-                            let mut replica_streams = server_state
-                                .replica_connections
-                                .lock()
-                                .map_err(|e| anyhow!("Failed to lock replica connections: {}", e))?;
-                            for replica_stream in replica_streams.iter_mut() {
-                                debug!("Propagating command to replica: {:?}", &command_bytes);
-                                replica_stream.write_all(&command_bytes)?
-                            }
-                        }
-                    }
+                    handle_command(
+                        stream,
+                        &received_message,
+                        elements,
+                        storage,
+                        server_state,
+                        should_reply,
+                    )?;
                 }
                 DataType::Rdb { value } => {
-                    // Replica receiving RDB snapshot from master
-                    let maybe_received_storage = Storage::from_rdb(&value).ok();
-                    debug!("Received storage {:?}", &maybe_received_storage);
-                    if let Some(received_storage) = maybe_received_storage {
-                        let mut storage = storage.lock().map_err(|e| anyhow!("Failed to lock storage: {}", e))?;
-                        for (key, value) in received_storage.data.into_iter() {
-                            storage.data.insert(key, value);
-                        }
-                    }
+                    handle_rdb_snapshot(value, storage)?;
                 }
                 DataType::SimpleString { value: _ } => {
-                    // Replica receiving FULLRESYNC response
-                    let string_content = received_message.as_string()?;
-                    if string_content.starts_with("FULLRESYNC") {
-                        let reply_parts: Vec<&str> = string_content.split(' ').collect();
-                        let replication_id = reply_parts.get(1).ok_or(anyhow!(
-                            "Could not read replication_id from FULLRESYNC reply {:?}",
-                            string_content
-                        ))?;
-                        info!(
-                            "Received replication_id {} from the master",
-                            replication_id
-                        );
-                    }
+                    handle_simple_string(&received_message)?;
                 }
                 _ => (),
             }
         }
     }
+}
+
+fn handle_command(
+    stream: &mut TcpStream,
+    received_message: &DataType,
+    elements: &[DataType],
+    storage: &Arc<Mutex<Storage>>,
+    server_state: &Arc<ServerState>,
+    should_reply: bool,
+) -> Result<(), anyhow::Error> {
+    let command_name = commands::parse_command_name(received_message)?;
+    let Some(command) = build_command(&command_name, received_message, elements, server_state)
+    else {
+        return Ok(());
+    };
+
+    if command_name == "PSYNC" {
+        register_replica(stream, server_state)?;
+    }
+
+    let reply = command.execute(storage)?;
+    if should_reply || command.should_always_reply() {
+        send_reply(stream, reply)?;
+    }
+
+    if server_state.is_master() && command.is_propagated_to_replicas() {
+        propagate_to_replicas(&*command, server_state)?;
+    }
+
+    Ok(())
+}
+
+fn build_command(
+    command_name: &str,
+    received_message: &DataType,
+    elements: &[DataType],
+    server_state: &Arc<ServerState>,
+) -> Option<Box<dyn RedisCommand>> {
+    let message = received_message.clone();
+    let state = || Arc::clone(server_state);
+    let notifier = || Arc::clone(&server_state.blocking_notifier);
+
+    let command: Box<dyn RedisCommand> = match command_name {
+        "ECHO"     => Box::new(Echo { message, argument: elements.get(1).cloned() }),
+        "PING"     => Box::new(Ping { message }),
+        "SET"      => Box::new(Set { message }),
+        "GET"      => Box::new(Get { message }),
+        "COMMAND"  => Box::new(Command { message }),
+        "INFO"     => Box::new(Info { message, server_state: state() }),
+        "REPLCONF" => Box::new(ReplConf { message, server_state: state() }),
+        "RPUSH"    => Box::new(RPush { message, notifier: notifier() }),
+        "LPUSH"    => Box::new(LPush { message, notifier: notifier() }),
+        "LRANGE"   => Box::new(LRange { message }),
+        "LLEN"     => Box::new(LLen { message }),
+        "LPOP"     => Box::new(LPop { message }),
+        "BLPOP"    => Box::new(BLPop { message, notifier: notifier() }),
+        "TYPE"     => Box::new(Type { message }),
+        "PSYNC"    => Box::new(PSync { message, server_state: state() }),
+        _ => return None,
+    };
+    Some(command)
+}
+
+fn send_reply(stream: &mut TcpStream, reply: Vec<DataType>) -> Result<(), anyhow::Error> {
+    for message in reply.into_iter() {
+        trace!("Sending: {:?}", message);
+        let message_bytes = message.serialize();
+        trace!("which serializes to {:?}", message_bytes);
+        stream.write_all(&message_bytes)?;
+    }
+    Ok(())
+}
+
+fn propagate_to_replicas(
+    command: &dyn RedisCommand,
+    server_state: &Arc<ServerState>,
+) -> Result<(), anyhow::Error> {
+    let command_bytes = command.serialize();
+    let mut replica_streams = server_state
+        .replica_connections
+        .lock()
+        .map_err(|e| anyhow!("Failed to lock replica connections: {}", e))?;
+    for replica_stream in replica_streams.iter_mut() {
+        debug!("Propagating command to replica: {:?}", &command_bytes);
+        replica_stream.write_all(&command_bytes)?;
+    }
+    Ok(())
+}
+
+fn register_replica(
+    stream: &TcpStream,
+    server_state: &Arc<ServerState>,
+) -> Result<(), anyhow::Error> {
+    server_state
+        .replica_connections
+        .lock()
+        .map_err(|e| anyhow!("Failed to lock replica connections: {}", e))?
+        .push(stream.try_clone()?);
+    Ok(())
+}
+
+fn handle_rdb_snapshot(
+    value: &[u8],
+    storage: &Arc<Mutex<Storage>>,
+) -> Result<(), anyhow::Error> {
+    let maybe_received_storage = Storage::from_rdb(value).ok();
+    debug!("Received storage {:?}", &maybe_received_storage);
+    if let Some(received_storage) = maybe_received_storage {
+        let mut storage = storage
+            .lock()
+            .map_err(|e| anyhow!("Failed to lock storage: {}", e))?;
+        for (key, value) in received_storage.data.into_iter() {
+            storage.data.insert(key, value);
+        }
+    }
+    Ok(())
+}
+
+fn handle_simple_string(received_message: &DataType) -> Result<(), anyhow::Error> {
+    let string_content = received_message.as_string()?;
+    if string_content.starts_with("FULLRESYNC") {
+        let reply_parts: Vec<&str> = string_content.split(' ').collect();
+        let replication_id = reply_parts.get(1).ok_or_else(|| {
+            anyhow!(
+                "Could not read replication_id from FULLRESYNC reply {:?}",
+                string_content
+            )
+        })?;
+        info!("Received replication_id {} from the master", replication_id);
+    }
+    Ok(())
 }
 
 #[cfg(test)]
